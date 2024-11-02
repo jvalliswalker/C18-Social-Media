dc1015effb9744ac0c753693a36ae72cbb31ea42
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-01 21:15:08 -0700

    Create PUT and DELETE endpoints, add various comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,21 +2,66 @@ const router = require('express').Router();
 const User = require('../../models/User');
 const { awaitWithCatch } = require('../../utils/controllerUtils');
 
+const invalidBodyMessage = 'Invalid user data in request body';
+
 // Request all User documents
 router.get('/', async (req, res) => {
+  // Request all Users
   const { result, code } = await awaitWithCatch(User.find({}));
 
+  // Send results
   res.status(code).send(result);
 });
 
+// Request User by Id
 router.get('/:userId', async (req, res) => {
+  // Request single User by Id
   const { result, code } = await awaitWithCatch(User.find(
     {
       _id: req.params.userId
     }
   ))
 
+  // Send results
   res.status(code).send(result);
+})
+
+router.put('/:userId', async (req, res) => {
+
+  // Check that at least one property is present, else return error
+  const { username, email, thoughts } = req.body;
+
+  if(!username && !email && !thoughts){
+    res.status(500).json({ message: invalidBodyMessage });
+  }
+
+  // Build updater object
+  const updater = {}
+
+  if(username){ updater.username = username; }
+  if(email){ updater.email = email; }
+  if(thoughts){ updater.thoughts = thoughts; }
+
+  // Attempt Update
+  const { result, code } = await awaitWithCatch(User.findOneAndUpdate(
+    { _id: req.params.userId },
+    updater,
+    {new: true}
+  ))
+
+  // Send results
+  res.status(code).json(result);
+})
+
+// Delete single User by Id
+router.delete('/:userId', async (req, res) => {
+  // Attempt User delete
+  const { result, code } = await awaitWithCatch(User.deleteOne(
+    { _id: req.params.userId }
+  ))
+
+  // Send response
+  res.status(code).json(result);
 })
 
 // Create User from POST body
@@ -27,7 +72,7 @@ router.post('/', async (req, res) => {
   
   if(!username || !email){
     res.status(400).json(
-      { message: 'Invalid user data in POST request body'}
+      { message: invalidBodyMessage }
     )
   }
 
