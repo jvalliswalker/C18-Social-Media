5951992752694a14d4f8817753f79a014af7a9ad
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-01 20:45:59 -0700

    Create general promise/error handler  function

---
+++ utils/controllerUtils.js
@@ -0,0 +1,12 @@
+// General promise and error handler
+function awaitWithCatch(promise){
+  return promise
+    .then(data => {
+      return {result: data, code: 200};
+    })
+    .catch(error => {
+      return {result: error, code: 500};
+    })
+}
+
+module.exports = { awaitWithCatch }
