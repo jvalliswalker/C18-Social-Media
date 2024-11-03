537aa28911ad427c3db18ab11fe32b37e79b94b6
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-03 10:27:33 -0800

    Remove standard Id

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 
-const reactionSchema = new mongoose.Schema({
-  reactionId: {
-    type: mongoose.Types.ObjectId,
-    default: new mongoose.Types.ObjectId(),
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Types.ObjectId,
+      default: new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
   },
-  reactionBody: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-});
+  { _id: false }
+);
 
 reactionSchema.virtual("createdDate").get(() => {
   return this.createdAt;
